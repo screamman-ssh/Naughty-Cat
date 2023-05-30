184036ed191c3b1ba9c027c2edcd5cc6760bd071
Sahachat Hukhiao <autogran41@gmail.com>
2023-05-30 12:18:38 +0700

    refactor code by encapsulation + add type hint

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ Start overlay screen (cat screen)
 """
 def start_overlay_screen() -> None:
     #Start animate sprite
-    sprite = Sprite(window=overlayWin, root=root)
+    sprite : Sprite = Sprite(window=overlayWin, root=root)
     root.withdraw()
     overlayWin.deiconify()
     overlayWin.state('zoomed')
     sprite.drag_stop()
-    print(sprite.canvas)
     overlayWin.after(400, sprite.check_stop)
 
 """
@@ -21,7 +20,7 @@ Create window
 """
 if __name__ == "__main__":
     #Create root window
-    root = Tk()
+    root : Tk = Tk()
     root.geometry("255x90")
     root.title("Naughty Cat")
     root.resizable(0,0)
@@ -29,19 +28,19 @@ if __name__ == "__main__":
     #Root/main window widget and layout
     root.grid_rowconfigure(1, weight=1)
     root.grid_columnconfigure(1, weight=1)
-    mainFrame = ttk.Frame(root).grid(row=1, sticky="nsew")
-    image = Image.open(path("icon.png"))
-    image = image.resize((30, 30))
-    photo = ImageTk.PhotoImage(image)
-    img = ttk.Label(mainFrame, image=photo)
-    img.place(relx=0.15, rely=0.1)
-    titleLabel = ttk.Label(mainFrame, text="Naughty Cat", font=('Arial', 18))
+    mainFrame : ttk.Frame = ttk.Frame(root).grid(row=1, sticky="nsew")
+    iconImg : Image  = Image.open(path("icon.png"))
+    iconImg = iconImg.resize((30, 30))
+    icon: any = ImageTk.PhotoImage(iconImg)
+    iconLabel : ttk.Label = ttk.Label(mainFrame, image=icon)
+    iconLabel.place(relx=0.15, rely=0.1)
+    titleLabel : ttk.Label = ttk.Label(mainFrame, text="Naughty Cat", font=('Arial', 18))
     titleLabel.place(relx=0.3, rely=0.13)
-    initOverlayButton = ttk.Button(mainFrame, text="Start", command=start_overlay_screen)
+    initOverlayButton : ttk.Button = ttk.Button(mainFrame, text="Start", command=start_overlay_screen)
     initOverlayButton.place(relx=0.5, rely=0.7, anchor=CENTER )
 
     #Create overlay window
-    overlayWin = Toplevel(root)
+    overlayWin : Toplevel = Toplevel(root)
     #Set transparent and click-throught window
     overlayWin.config(bg='#2E2E8B')
     overlayWin.attributes('-transparentcolor', '#2E2E8B', '-topmost', 1)
--- sprite.py
+++ sprite.py
@@ -21,48 +21,49 @@ def path(target: str) -> str:
 Class Sprite module
 """
 class Sprite:
-    def __init__(self, window, root) -> None:
+    def __init__(self, window: Toplevel, root: Tk) -> None:
         self.__load_image()
         #Create sprite canvas
-        self.root = root
-        self.window = window
-        self.sprite_width : int = self.sprite_img_dict["walk_l"][0].width()
-        self.sprite_height : int = self.sprite_img_dict["walk_l"][0].height()
-        self.canvas = Canvas(self.window, width=self.sprite_width + 30, height=self.sprite_height, bg='#2E2E8B', highlightthickness=0)
-        self.canvas.place(x=200, y=self.window.winfo_screenheight() - self.sprite_height)
-        self.sprite = self.canvas.create_image(0, 0, anchor='nw', image=self.sprite_img_dict["walk_l"][0])
-        self.canvas.prevX: int = 500
-        self.canvas.prevY: int = self.window.winfo_screenheight() - self.sprite_height
-        self.canvas.state: str = "move"
+        self.__root : Tk = root
+        self.__window : Toplevel = window
+        self.sprite_width : int = self.__sprite_img_dict["walk_l"][0].width()
+        self.sprite_height : int = self.__sprite_img_dict["walk_l"][0].height()
+        self.__canvas : Canvas = Canvas(self.__window, width=self.sprite_width + 30, height=self.sprite_height, bg='#2E2E8B', highlightthickness=0)
+        self.__canvas.place(x=200, y=self.__window.winfo_screenheight() - self.sprite_height)
+        self.__sprite = self.__canvas.create_image(0, 0, anchor='nw', image=self.__sprite_img_dict["walk_l"][0])
+        self.__canvas.prevX: int = 500
+        self.__canvas.prevY: int = self.__window.winfo_screenheight() - self.sprite_height
+        self.__canvas.state: str = "move"
         #Sprite binding
-        self.canvas.bind("<Button-1>", self.drag_start)
-        self.canvas.bind("<B1-Motion>", self.drag_motion)
-        self.canvas.bind("<ButtonRelease-1>", self.drag_stop)
-        self.canvas.bind("<Button-3>", self.right_click_popup)
+        self.__canvas.bind("<Button-1>", self.drag_start)
+        self.__canvas.bind("<B1-Motion>", self.drag_motion)
+        self.__canvas.bind("<ButtonRelease-1>", self.drag_stop)
+        self.__canvas.bind("<Button-3>", self.right_click_popup)
         # canvas.bind("<Leave>", mouse_leave)
-        self.create_ctx_menu()
+        #Create context menu
+        self.__create_ctx_menu()
 
     def __load_image(self) -> None:
         #Set image list by behavior
-        self.sprite_img_detail : dict[str, int] = {"walk_l": 7, "walk_r": 7, "idle": 4, "sit": 6, "love": 6, "drag": 5, "drag_idle": 4}
-        self.sprite_img_dict : dict[str, list[any]] = {}
-        for behave, frame in self.sprite_img_detail.items():
-            self.sprite_img_dict[behave] = [PhotoImage(file=path(f"asset\sprite_{behave}_{i}.png"))  for i in range(0, frame)]
+        self.__sprite_img_detail : dict[str, int] = {"walk_l": 7, "walk_r": 7, "idle": 4, "sit": 6, "love": 6, "drag": 5, "drag_idle": 4}
+        self.__sprite_img_dict : dict[str, list[any]] = {}
+        for behave, frame in self.__sprite_img_detail.items():
+            self.__sprite_img_dict[behave] = [PhotoImage(file=path(f"asset\sprite_{behave}_{i}.png"))  for i in range(0, frame)]
         
-    def create_ctx_menu(self) -> None:
+    def __create_ctx_menu(self) -> None:
         #Right click menu
-        self.ctx_menu = Menu(self.window, tearoff=0)
-        self.ctx_menu.add_command(label="Pet", command=self.pet)
-        self.ctx_menu.add_separator()
-        self.ctx_menu.add_command(label="Open Main Window", command=self.open_main_window)
-        self.ctx_menu.add_command(label="Close", command=self.root.destroy)
+        self.__ctx_menu : Menu = Menu(self.__window, tearoff=0)
+        self.__ctx_menu.add_command(label="Pet", command=self.pet)
+        self.__ctx_menu.add_separator()
+        self.__ctx_menu.add_command(label="Open Main Window", command=self.open_main_window)
+        self.__ctx_menu.add_command(label="Close", command=self.__root.destroy)
 
     """
     Back to main window
     """
     def open_main_window(self) -> None:
-        self.root.deiconify()
-        self.window.withdraw()
+        self.__root.deiconify()
+        self.__window.withdraw()
     """
     Sprite's movement and random behavior
     """
@@ -78,14 +79,14 @@ class Sprite:
             step: int = -2
         while True:
             # mouse_pos()
-            if (self.canvas.state == "move"):
+            if (self.__canvas.state == "move"):
                 #Change direction when get to leftmost and rightmost of screen
-                if (self.canvas.prevX < 0):
+                if (self.__canvas.prevX < 0):
                     if (step < 0):
                         step *= -1
                     randBehave = 1
                     # print("move right")
-                elif (self.canvas.prevX > self.window.winfo_screenwidth()):
+                elif (self.__canvas.prevX > self.__window.winfo_screenwidth()):
                     if (step > 0):
                         step *= -1
                     randBehave = 0
@@ -113,11 +114,11 @@ class Sprite:
                     behaveStr = "walk_l"
                     step = -2
                 #Update canvas
-                frame = (frameCounter % self.sprite_img_detail[behaveStr])
-                self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict[behaveStr][frame])
-                self.canvas.prevX += step
-                self.canvas.place(x=self.canvas.prevX)
-                self.window.update()
+                frame = (frameCounter % self.__sprite_img_detail[behaveStr])
+                self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict[behaveStr][frame])
+                self.__canvas.prevX += step
+                self.__canvas.place(x=self.__canvas.prevX)
+                self.__window.update()
                 frameCounter += 1
                 time.sleep(0.1)
             else:
@@ -129,74 +130,74 @@ class Sprite:
     stopFrameCount: int = 0
     def check_stop(self):
         global stopFrameCount
-        if (self.canvas.prevX == self.canvas.winfo_x() and self.canvas.prevY == self.canvas.winfo_y()):
-            self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag_idle"][stopFrameCount % 4])
+        if (self.__canvas.prevX == self.__canvas.winfo_x() and self.__canvas.prevY == self.__canvas.winfo_y()):
+            self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag_idle"][stopFrameCount % 4])
             #Count stop frame for drag idle animation
             stopFrameCount += 1
             print(f"Move Stop {stopFrameCount}")
-        self.window.after(400, self.check_stop)    
+        self.__window.after(400, self.check_stop)    
 
     """
     Drag when start click event handler
     """
     def drag_start(self, event) -> None:
-        self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][0])
-        self.canvas.startX = event.x
-        self.canvas.startY = event.y
-        self.canvas.prevX = self.canvas.winfo_x()
-        self.canvas.prevY = self.canvas.winfo_y()
+        self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][0])
+        self.__canvas.startX = event.x
+        self.__canvas.startY = event.y
+        self.__canvas.prevX = self.__canvas.winfo_x()
+        self.__canvas.prevY = self.__canvas.winfo_y()
         # canvas.state = "drag_start"
 
     """
     Drag move event handler
     """
     def drag_motion(self, event) -> None:
-        x = self.canvas.winfo_x() - self.canvas.startX + event.x
-        y = self.canvas.winfo_y() - self.canvas.startY + event.y
-        self.canvas.state = "drag_move"
-        self.canvas.place(x=x, y=y)
+        x = self.__canvas.winfo_x() - self.__canvas.startX + event.x
+        y = self.__canvas.winfo_y() - self.__canvas.startY + event.y
+        self.__canvas.state = "drag_move"
+        self.__canvas.place(x=x, y=y)
         #Set zero to stop frame
         global stopFrameCount
         stopFrameCount = 0
         #Detect which direction sprite has been move by draging
-        if (self.canvas.prevX + 8 < x):
-            # self.canvas.state = "move right"
-            self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][2])
-        elif (self.canvas.prevX - 8 > x):
-            # self.canvas.state = "move left"
-            self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][4])
-        elif (self.canvas.prevX + 3 < x):
-            self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][1])
+        if (self.__canvas.prevX + 8 < x):
+            # self.__canvas.state = "move right"
+            self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][2])
+        elif (self.__canvas.prevX - 8 > x):
+            # self.__canvas.state = "move left"
+            self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][4])
+        elif (self.__canvas.prevX + 3 < x):
+            self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][1])
             # print("Slightly Move Right")
-        elif (self.canvas.prevX - 3 > x):
-            self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][3])
+        elif (self.__canvas.prevX - 3 > x):
+            self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][3])
             # print("Slightly Move Left")
-        if (self.canvas.prevY < y):
+        if (self.__canvas.prevY < y):
             print("Move Down")
-        elif (self.canvas.prevY > y):
+        elif (self.__canvas.prevY > y):
             print("Move Up")
-        self.canvas.prevX = x
-        self.canvas.prevY = y
+        self.__canvas.prevX = x
+        self.__canvas.prevY = y
 
     """
     Stop dragging / Button leave event handler
     """
     def drag_stop(self, event=None) -> None:
-        self.bottom: int = self.window.winfo_screenheight() - self.sprite_img_dict["drag"][0].height()
-        currentY: int = self.canvas.prevY
-        self.canvas.state = "drag_stop"
-        print(f"{self.canvas.prevY} {self.bottom}")
+        self.bottom: int = self.__window.winfo_screenheight() - self.__sprite_img_dict["drag"][0].height()
+        currentY: int = self.__canvas.prevY
+        self.__canvas.state = "drag_stop"
+        print(f"{self.__canvas.prevY} {self.bottom}")
         while True:
             #Fall event and animation
-            if (self.bottom > currentY and self.canvas.state != "drag_start"):
-                self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["drag"][0])
+            if (self.bottom > currentY and self.__canvas.state != "drag_start"):
+                self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["drag"][0])
                 currentY += 5
-                self.canvas.place(x=self.canvas.winfo_x(), y=currentY)
-                self.window.update()
+                self.__canvas.place(x=self.__canvas.winfo_x(), y=currentY)
+                self.__window.update()
                 # time.sleep(0.05)
-            elif (self.canvas.state != "drag_move"):
+            elif (self.__canvas.state != "drag_move"):
                 currentY = self.bottom
-                self.canvas.state = "move"
+                self.__canvas.state = "move"
                 self.move()
             else:
                 break
@@ -205,12 +206,12 @@ class Sprite:
     Pet animation
     """
     def pet(self) -> None:
-        self.canvas.state = "pet"
+        self.__canvas.state = "pet"
         for i in range(0, 30):
-            if (self.canvas.state != "drag_move"):
+            if (self.__canvas.state != "drag_move"):
                 time.sleep(0.1)
-                self.canvas.itemconfig(self.sprite, image=self.sprite_img_dict["love"][i % 6])
-                self.window.update()
+                self.__canvas.itemconfig(self.__sprite, image=self.__sprite_img_dict["love"][i % 6])
+                self.__window.update()
             else:
                 break
 
@@ -223,6 +224,6 @@ class Sprite:
     """
     def right_click_popup(self, event) -> None:
         try:
-            self.ctx_menu.tk_popup(event.x_root, event.y_root)
+            self.__ctx_menu.tk_popup(event.x_root, event.y_root)
         finally:
-            self.ctx_menu.grab_release()
+            self.__ctx_menu.grab_release()
